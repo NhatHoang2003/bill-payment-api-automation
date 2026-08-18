7f7b7c366ff9db4e1e98feeb42960464f9cde21a
Odoriko <hoangle8122003@gamil.com>
2026-08-18 23:50:49 +0700

    ci/cd test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from '@playwright/test';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * Playwright API Automation Configuration
